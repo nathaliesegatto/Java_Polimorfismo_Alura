bfced6e51f2c0c4afb4bc7370085f7046d4a087c
Nathalie Segatto Morel <nmorel@cleartech.com.br>
2022-04-26 16:47:27 -0300

    Arquivos aula 1 adicionados

---
+++ TesteGerente.java
@@ -0,0 +1,20 @@
+
+public class TesteGerente {
+
+	public static void main(String[] args) {
+		Gerente g1 = new Gerente();
+		g1.setNome("Marco");
+		g1.setCpf("223342513");
+		g1.setSalario(5000.0);
+		
+		System.out.println("Nome: " + g1.getNome());
+		System.out.println("CPF: " + g1.getCpf());
+		System.out.println("Salário: R$ " + g1.getSalario());
+		
+		boolean autenticou = g1.autentica(23465);
+		
+		System.out.println("Senha correta: " + autenticou);
+		
+	}
+
+}
